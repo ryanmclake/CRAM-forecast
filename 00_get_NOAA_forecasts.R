68561af1089b2dd6fcc4d1dd7a3d7cd1014cc6d2
Ryan McClure <ryan333@vt.edu>
2021-03-01 17:08:39 -0500

    thats a lot of forecasts

--- 00_get_NOAA_forecasts.R
+++ 00_get_NOAA_forecasts.R
@@ -11,12 +11,16 @@ download_noaa_files_s3 <- function(siteID, date, cycle, local_directory){
     aws.s3::save_object(object[[i]], bucket = "drivers", file = file.path(local_directory, object[[i]]$Key))
   }
 }
+date = seq(from = as.Date("2021-03-01"), to = as.Date(Sys.Date()), by = "days")
+cycle = c("00")
 
-Sys.setenv("AWS_DEFAULT_REGION" = "data",
-           "AWS_S3_ENDPOINT" = "ecoforecast.org")
+for(i in 1:length(date)){
+  for(g in 1:length(cycle)){
+  download_noaa_files_s3(siteID = "CRAM", 
+                         date = date[i], 
+                         cycle = cycle[g], 
+                         local_directory <- "C:/Users/Owner/Documents/CRAM-forecast/NOAA_data/")
 
-object <- aws.s3::get_bucket("drivers", prefix=paste0("noaa/NOAAGEFS_1hr/CRAM/2020-11-10/00"))
-
-
-download_noaa_files_s3(siteID = "CRAM", date = "2020-11-10", cycle = "00", local_directory <- "/home/ryan333/CRAM-forecast/NOAA_data/")
+}
+}
 
