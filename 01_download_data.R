94efdb794b36deb991ec675dedef5be14fb9bef6
Ryan Paul McClure <ryan333@ca013.ca.arc.internal>
2021-03-12 09:48:29 -0500

    big changes to CRAM-forecast workflow

--- 01_download_data.R
+++ 01_download_data.R
@@ -1,11 +1,170 @@
-# set your WD paths for CRAM FLARE
+#download.file("https://github.com/cwida/duckdb/releases/download/master-builds/duckdb_r_src.tar.gz", destfile = "./duckdb_r_src.tar.gz")
+#install.packages("duckdb_r_src.tar.gz", repo = NULL)
 
+remotes::install_github("cboettig/neonstore")
+remotes::install_github("eco4cast/EFIstandards")
+remotes::install_github("rqthomas/flare")
+
+if (!require('pacman')) install.packages('pacman'); library('pacman')
+pacman::p_load(tidyverse, lubridate, VIM, naniar, missMDA, Amelia, mice, FactoMineR, broom, aws.s3)
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+### DOANLOAD THE NEWEST NOAA DATA ###
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+source(file.path(lake_directory, "data_download/NOAA_downloads.R"))
+
+# Specify your dates that you want NOAA forecast for
+date = seq(from = as.Date(Sys.Date()-16), to = as.Date(Sys.Date()-1), by = "days")
+cycle = c("00")
+
+# Download the forecast from the MinIO site
+for(i in 1:length(date)){
+  for(g in 1:length(cycle)){
+    download_noaa_files_s3(siteID = siteID,
+                           date = date[i], 
+                           cycle = cycle[g], 
+                           local_directory <- "home/ryan333/CRAM-forecast/data/NOAA_data/")
+  }
+}
+
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+### DOANLOAD THE NEWEST NEON DATA ###
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+neonstore::neon_dir()
+Sys.setenv("NEONSTORE_HOME" = "/groups/rqthomas_lab/neonstore_cram")
+neonstore::neon_dir()
+
+# Lake and tower met station download
+met_products = c("DP1.00098.001", "DP1.00002.001", "DP1.00023.001", "DP1.00006.001", "DP1.00001.001", "DP1.00004.001")
+
+# Download newest met products
+neonstore::neon_download(product = met_products, site = c("CRAM","UNDE"))
+
+# Store the NEON met data products
+neonstore::neon_store("SECPRE_30min-expanded")
+neonstore::neon_store("2DWSD_30min-expanded")
+neonstore::neon_store("SLRNR_30min-expanded")
+neonstore::neon_store("SAAT_30min-expanded")
+neonstore::neon_store("RH_30min-expanded")
+neonstore::neon_store("BP_30min-basic")
+
+
+# Airtemp
+airtemp <- neonstore::neon_table(table = "SAAT_30min-expanded", site = "CRAM") %>%
+  select(endDateTime, tempSingleMean)%>%
+  mutate(time = lubridate::floor_date(endDateTime, unit = "hour"))%>%
+  select(-endDateTime)%>%
+  group_by(time) %>%
+  summarize_at(c("tempSingleMean"), mean, na.rm = TRUE)%>%
+  arrange(time)%>%
+  mutate(time = time - 6*3600)
+
+# Radiation
+radiation <- neonstore::neon_table(table = "SLRNR_30min-expanded", site = "CRAM") %>%
+  select(endDateTime, inSWMean, inLWMean) %>%
+  mutate(time = lubridate::floor_date(endDateTime, unit = "hour"))%>%
+  select(-endDateTime)%>%
+  group_by(time) %>%
+  summarize_at(c("inSWMean", "inLWMean"), mean, na.rm = TRUE)%>%
+  arrange(time)%>%
+  mutate(time = time - 6*3600)
+
+# Humidity
+humidity <- neonstore::neon_table(table = "RH_30min-expanded", site = "CRAM") %>% 
+  select(endDateTime, RHMean)%>%
+  mutate(time = lubridate::floor_date(endDateTime, unit = "hour"))%>%
+  select(-endDateTime)%>%
+  group_by(time) %>%
+  summarize_at(c("RHMean"), mean, na.rm = TRUE)%>%
+  arrange(time)%>%
+  mutate(time = time - 6*3600)
+
+# Precipitation
+precip  <- neonstore::neon_table(table = "SECPRE_30min-expanded", site = "UNDE") %>%
+  select(endDateTime, secPrecipBulk) %>%
+  mutate(time = lubridate::floor_date(endDateTime, unit = "hour"))%>%
+  select(-endDateTime)%>%
+  group_by(time) %>%
+  summarize_at(c("secPrecipBulk"), sum, na.rm = TRUE)%>%
+  arrange(time)%>%
+  mutate(time = time - 6*3600)
+
+# Wind Speed
+windspeed <- neonstore::neon_table(table = "2DWSD_30min-expanded", site = "CRAM")%>%  
+  select(endDateTime, windSpeedMean)%>%
+  mutate(time = lubridate::floor_date(endDateTime, unit = "hour"))%>%
+  select(-endDateTime)%>%
+  group_by(time) %>%
+  summarize_at(c("windSpeedMean"), sum, na.rm = TRUE)%>%
+  arrange(time)%>%
+  mutate(time = time - 6*3600)
+
+# Pressure
+pressure <- neonstore::neon_table(table = "BP_30min-basic", site = "CRAM") %>%
+  select(endDateTime, staPresMean)%>%
+  mutate(time = lubridate::floor_date(endDateTime, unit = "hour"))%>%
+  select(-endDateTime)%>%
+  group_by(time) %>%
+  summarize_at(c("staPresMean"), mean, na.rm = TRUE)%>%
+  arrange(time)%>%
+  mutate(time = time - 6*3600)
+
+
+met_target <- full_join(radiation, airtemp, by = "time")%>%
+  full_join(., humidity, by = "time")%>%
+  full_join(., windspeed, by = "time")%>%
+  full_join(., precip, by = "time")%>%
+  full_join(., pressure, by = "time")%>%
+  rename(ShortWave = inSWMean, LongWave = inLWMean, AirTemp = tempSingleMean,
+         RelHum = RHMean, WindSpeed = windSpeedMean, Rain = secPrecipBulk, Pressure = staPresMean)%>%
+  mutate(Rain = Rain*0.024)%>%
+  mutate(Pressure = Pressure*1000)%>%
+  mutate(ShortWave = ifelse(ShortWave<=0,0,ShortWave))%>%
+  filter(time >= "2018-08-06")
+
+met_target <- as.data.frame(met_target)
+
+write_csv(met_target, "./data/met_data_w_gaps.csv")
+
+
+# Lake water temperature
+buoy_products = c("DP1.20264.001")
+
+# Download newest buoy  products
+neonstore::neon_download(product = buoy_products, site = "CRAM")
+
+# Store the NEON buoy data products
+neonstore::neon_store("TSD_30_min-expanded")
+
+# Water temperature by depth
+# ----------------------------------------------------------------------------------------
+water_temp <- neonstore::neon_table(table = "TSD_30_min-expanded", site = "CRAM")%>% 
+  select(endDateTime, thermistorDepth, tsdWaterTempMean) %>%
+  arrange(endDateTime, thermistorDepth)%>%
+  rename(depth = thermistorDepth)%>%
+  rename(value = tsdWaterTempMean)%>%
+  rename(timestamp = endDateTime)%>%
+  mutate(variable = "temperature",
+         method = "thermistor",
+         value = ifelse(is.nan(value), NA, value))%>%
+  select(timestamp, depth, value, variable, method)
+
+write_csv(water_temp, "./data/temp_data.csv")
+
+
+# set your WD paths for CRAM FLARE
 lake_directory <- "/home/ryan333/CRAM-forecast"
 qaqc_data_location <- file.path(lake_directory, "qaqc_data")
 data_location <- "/home/ryan333/CRAM-forecast"
-noaa_data_location <- "/home/ryan333/CRAM-forecast/NOAA_data/noaa/NOAAGEFS_1hr"
+noaa_data_location <- "/home/ryan333/CRAM-forecast/data/NOAA_data/noaa/NOAAGEFS_1hr"
 if(!dir.exists(qaqc_data_location)){dir.create(qaqc_data_location)}
-
 
 # See configured YAML file for CRAMPTON --> Different from FCR!
 config <- yaml::read_yaml(file.path(lake_directory,"data_processing", "observation_processing.yml"))
@@ -23,7 +182,6 @@ if(!file.exists(file.path(config$noaa_data_location, config$noaa_location))){
   stop("Missing NOAA forecast GitHub repo")
 }
 
-
 setwd(file.path(config$data_location, config$realtime_met_station_location))
 system(paste0("git pull"))
 
@@ -32,4 +190,6 @@ system(paste0("git pull"))
 
 setwd(file.path(config$noaa_data_location, config$noaa_location))
 system(paste0("git pull"))
+
+
 
--- data_processing/R/met_qaqc.R
+++ data_processing/R/met_qaqc.R
@@ -20,11 +20,104 @@ met_qaqc <- function(realtime_file,
     
   }else{
     
-    d1 <- readr::read_csv(realtime_file)
-    d1$time <- lubridate::force_tz(d1$time, tzone = "CST6CDT") #input_file_tz
-    
-    d <- d1
+    d <- readr::read_csv(realtime_file)
   }
+  
+  d <- as.data.frame(d)
+  
+  
+  # Fill in missing data (Used Amelia II instead of NLDAS)
+  # This way all data are solely derived from the NEON data platforms
+  
+  #ShortWave
+  amelia.sw <- amelia(d, m = 50, polytime = 2, ts = "time", cs = NULL, lags = "ShortWave", leads = "ShortWave")
+  sw_imputations <- bind_rows(unclass(amelia.sw$imputations), .id = "m") %>%
+    select(time, ShortWave)%>%
+    group_by(time)%>%
+    summarise_all(funs(mean))%>%
+    mutate(ShortWave = ifelse(ShortWave <= 0, 0, ShortWave))
+  
+  #LongWave
+  amelia.lw <- amelia(d, m = 50, polytime = 2, ts = "time", cs = NULL, lags = "LongWave", leads = "LongWave")
+  lw_imputations <- bind_rows(unclass(amelia.lw$imputations), .id = "m") %>%
+    select(time, LongWave)%>%
+    group_by(time)%>%
+    summarise_all(funs(mean))
+  plot(lw_imputations$time, lw_imputations$LongWave)
+  
+  #AirTemp
+  amelia.at <- amelia(d, m = 50, polytime = 0, ts = "time", cs = NULL, lags = "AirTemp", leads = "AirTemp")
+  at_imputations <- bind_rows(unclass(amelia.at$imputations), .id = "m") %>%
+    select(time, AirTemp)%>%
+    group_by(time)%>%
+    summarise_all(funs(mean))
+  plot(at_imputations$time, at_imputations$AirTemp)
+  
+  #Himidity
+  amelia.rh <- amelia(d, m = 50, polytime = 2, ts = "time", cs = NULL, lags = "RelHum", leads = "RelHum")
+  rh_imputations <- bind_rows(unclass(amelia.rh$imputations), .id = "m") %>%
+    select(time, RelHum)%>%
+    group_by(time)%>%
+    summarise_all(funs(mean))%>%
+    mutate(RelHum = ifelse(RelHum >= 100, 100, RelHum))
+  plot(rh_imputations$time, rh_imputations$RelHum)
+  
+  #Rain
+  amelia.pr <- amelia(d, m = 50, polytime = 2, ts = "time", cs = NULL, lags = "Rain", leads = "Rain")
+  pr_imputations <- bind_rows(unclass(amelia.pr$imputations), .id = "m") %>%
+    select(time, Rain)%>%
+    group_by(time)%>%
+    summarise_all(funs(mean))%>%
+    mutate(Rain = ifelse(Rain <= 0, 0, Rain))
+  plot(pr_imputations$time, pr_imputations$Rain)
+  
+  #WindSpeed
+  amelia.ws <- amelia(d, m = 50, polytime = 2, ts = "time", cs = NULL, lags = "WindSpeed", leads = "WindSpeed")
+  ws_imputations <- bind_rows(unclass(amelia.ws$imputations), .id = "m") %>%
+    select(time, WindSpeed)%>%
+    group_by(time)%>%
+    summarise_all(funs(mean))%>%
+    mutate(WindSpeed = ifelse(WindSpeed <= 0, 0, WindSpeed))
+  plot(ws_imputations$time, ws_imputations$WindSpeed)
+  
+  #Pressure
+  amelia.p <- amelia(d, m = 50, polytime = 2, ts = "time", cs = NULL, lags = "Pressure", leads = "Pressure")
+  p_imputations <- bind_rows(unclass(amelia.p$imputations), .id = "m") %>%
+    select(time, Pressure)%>%
+    group_by(time)%>%
+    summarise_all(funs(mean))
+  plot(p_imputations$time, p_imputations$Pressure)
+  
+  
+  imputed <- left_join(sw_imputations, lw_imputations, by = "time")%>%
+    left_join(., at_imputations, by = "time")%>%
+    left_join(., rh_imputations, by = "time")%>%
+    left_join(., pr_imputations, by = "time")%>%
+    left_join(., ws_imputations, by = "time")%>%
+    left_join(., p_imputations, by = "time")
+  
+  
+  met_new <- left_join(d, imputed, by = "time")
+  
+  d <- met_new %>%
+    mutate(ShortWave.x = ifelse(is.na(ShortWave.x), ShortWave.y, ShortWave.x))%>%
+    mutate(LongWave.x = ifelse(is.na(LongWave.x), LongWave.y, LongWave.x))%>%
+    mutate(AirTemp.x = ifelse(is.na(AirTemp.x), AirTemp.y, AirTemp.x))%>%
+    mutate(RelHum.x = ifelse(is.na(RelHum.x), RelHum.y, RelHum.x))%>%
+    mutate(WindSpeed.x = ifelse(is.na(WindSpeed.x), WindSpeed.y, WindSpeed.x))%>%
+    mutate(Rain.x = ifelse(is.na(Rain.x), Rain.y, Rain.x))%>%
+    mutate(Pressure.x = ifelse(is.na(Pressure.x), Pressure.y, Pressure.x))%>%
+    select(time, ShortWave.x, LongWave.x, AirTemp.x, RelHum.x, WindSpeed.x, Rain.x, Pressure.x)%>%
+    rename(surface_downwelling_shortwave_flux_in_air = ShortWave.x, 
+           surface_downwelling_longwave_flux_in_air = LongWave.x, 
+           air_temperature = AirTemp.x, 
+           specific_humidity = RelHum.x, 
+           wind_speed = WindSpeed.x, 
+           precipitation_flux = Rain.x, 
+           air_pressure = Pressure.x)
+  
+  
+  d$time <- lubridate::force_tz(d$time, tzone = "CST6CDT") #input_file_tz
   
   
   wshgt <- 3
